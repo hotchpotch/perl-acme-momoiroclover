ba46e5e88297a50b7a2e0ae3f1dfe1d586345e4d
sugyan <sugi1982@gmail.com>
2011-09-06 10:37:32 +0900

    remove unnecessary space

--- lib/Acme/MomoiroClover/Members/MomotaKanako.pm
+++ lib/Acme/MomoiroClover/Members/MomotaKanako.pm
@@ -8,7 +8,7 @@ use base qw(Acme::MomoiroClover::Members::Base);
 sub info {
     return (
         family_name_ja => '百田',
-        first_name_ja  => '夏菜子 ',
+        first_name_ja  => '夏菜子',
         family_name_en => 'Momota',
         first_name_en  => 'Kanako',
         nick           => [qw(かなこ デコちゃん)],
